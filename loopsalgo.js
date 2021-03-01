0ebab4d1e6694bdfa3f3c420e12d0e035ea26645
hsk2ad <hsk2ad@virginia.edu>
2021-03-01 09:42:20 -0500

    JavaScript

---
+++ loopsalgo.js
@@ -0,0 +1,11 @@
+var i = 1;
+for(var i = 1; i < 20; i =i+2){
+    console.log(i);
+}
+
+var num = 1;
+var total = 0;
+for(var num = 1; num <= 6; num = num+ 1){
+    total = total + num;
+    console.log("Num: " + num + ", Sum: " + total);
+}
